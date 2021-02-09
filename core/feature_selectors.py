ec08c90ff29d3125ef5cd86dfac041f99631e045
s-a-nersisyan <s.a.nersisyan@gmail.com>
2021-02-09 21:34:38 +0000

    Initial commit: feature pre-selectors and selectors

---
+++ core/feature_selectors.py
@@ -0,0 +1,133 @@
+"""Functions for feature selection
+
+Every feature selection function takes input DataFrame, 
+annotation DataFrame, number n and some optional arguments, 
+and returns a list of n features which is
+a subset of DataFrame column names.
+
+Each feature selection function have the following signature:
+def feature_selector(df, ann, n, **kwargs):
+    # Code
+    return n_element_list_of_features
+"""
+
+
+import numpy as np
+
+from scipy.stats import spearmanr, ttest_ind
+
+
+def t_test(df, ann, n, datasets):
+    """Select n features with the lowest p-values according to t-test
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A pandas DataFrame whose rows represent samples
+        and columns represent features.
+    ann : pandas.DataFrame
+        DataFrame with annotation of samples. Three columns are mandatory:
+        Class (binary labels), Dataset (dataset identifiers) and 
+        Dataset type (training, filtration, validation).
+    n : int
+        Number of features to select.
+    datasets : array-like
+        List of dataset identifiers which should be used to calculate
+        test statistic. By default (None), union of all non-validation
+        datasets will be used.
+
+    Returns
+    -------
+    list
+        List of n features associated with the lowest p-values.
+    """
+
+    # By default, consider all datasets except validation ones
+    if not datasets:
+        datasets = np.unique(ann.loc[ann["Dataset type"] != "Validation", "Dataset"])
+    
+    samples = ann.loc[ann["Dataset"].isin(datasets)].index
+    df_subset = df.loc[samples]
+    ann_subset = ann.loc[samples]
+    X = df_subset.to_numpy()
+    y = ann_subset["Class"].to_numpy()
+
+    t_statistics, pvalues = ttest_ind(X[y == 0], X[y == 1], axis=0)
+    features = df_subset.columns
+
+    return [feature for feature, pvalue in sorted(zip(features, pvalues), key=lambda x: x[1])][:n]
+
+
+def spearman_correlation(df, ann, n, datasets):
+    """Select n features with the highest correlation with target label
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A pandas DataFrame whose rows represent samples
+        and columns represent features.
+    ann : pandas.DataFrame
+        DataFrame with annotation of samples. Three columns are mandatory:
+        Class (binary labels), Dataset (dataset identifiers) and 
+        Dataset type (training, filtration, validation).
+    n : int
+        Number of features to select.
+    datasets : array-like
+        List of dataset identifiers which should be used to calculate
+        correlation. By default (None), union of all non-validation
+        datasets will be used.
+
+    Returns
+    -------
+    list
+        List of n features associated with the highest absolute
+        values of Spearman correlation.
+    """
+
+    # By default, consider all datasets except validation ones
+    if not datasets:
+        datasets = np.unique(ann.loc[ann["Dataset type"] != "Validation", "Dataset"])
+    
+    samples = ann.loc[ann["Dataset"].isin(datasets)].index
+    df_subset = df.loc[samples]
+    ann_subset = ann.loc[samples]
+    X = df_subset.to_numpy()
+    y = ann_subset["Class"].to_numpy()
+
+    pvalues = [spearmanr(X[:, j], y).pvalue for j in range(X.shape[1])]
+    features = df_subset.columns
+
+    return [feature for feature, pvalue in sorted(zip(features, pvalues), key=lambda x: x[1])][:n]
+
+
+def from_file(df, ann, n, path_to_file, sep=None):
+    """Select first n features from a given file
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A pandas DataFrame whose rows represent samples
+        and columns represent features.
+    ann : pandas.DataFrame
+        DataFrame with annotation of samples. This argument is
+        actually not used by the function.
+    n : int
+        Number of features to select.
+    path_to_file : str
+        Path to a file which contains feature names in a first column.
+        Path can be absolute or relative to the current working directory.
+    sep : str
+        Separator string using to identify first column in a given file.
+        By default (None), any whitespace character will be used.
+
+    Returns
+    -------
+    list
+        List of first n entries in the intersection of features from a given file 
+        with a list of features from a given DataFrame.
+    """
+
+    with open(path_to_file, "r") as f:
+        features_from_file = [line.split(sep)[0] for line in f]
+    
+    return [feature for feature in features_from_file if feature in df.columns][:n]
