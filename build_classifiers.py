ab7a131a5eaa3f515cf642d10a80c0d47b5057e6
BokovGrigoriy <bokovgrigoriy@gmail.com>
2021-02-16 19:41:03 +0300

    Added printing of two statistical tables

--- build_classifiers.py
+++ build_classifiers.py
@@ -11,6 +11,28 @@ from core import preprocessors
 from core import classifiers
 from core import accuracy_scores
 
+def classifiers_num (df, scoring, threshold, datasets):
+    """Get number of classifiers that have scoring greater than threshold in given datasets
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A pandas DataFrame whose rows represent classifiers
+        and columns represent their quality scores.
+    scoring : str
+        Key from scoring_functions dict defining the scoring function.
+    threshold : float
+        A number defining threshold for classifier filtering.
+    datasets : list
+        List of dataset identifiers.
+    
+    Returns
+    -------
+    int
+        Number of classifiers
+    """
+    queries = [ "(out['{}'] > {})".format(f"{ds};{scoring}", threshold) for ds in datasets ]
+    return df[eval(" & ".join(queries))].shape[0]
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -36,6 +58,8 @@ if __name__ == "__main__":
     df = pd.read_csv(os.path.join(config_dirname, config["data_path"]), index_col=0)
     ann = pd.read_csv(os.path.join(config_dirname, config["annotation_path"]), index_col=0)
     n_k = pd.read_csv(os.path.join(config_dirname, config["n_k_path"]))
+    main_scoring_function = config["main_scoring_function"]
+    main_scoring_threshold = config["main_scoring_threshold"]
 
     model = classification.ExhaustiveClassification(
         df, ann, n_k,
@@ -54,3 +78,34 @@ if __name__ == "__main__":
     )
     out = model.exhaustive_run()
     print(out)
+
+    table = pd.DataFrame(columns=['n','k','train_filtr','train_filtr_valid','percent'])
+    for n, k in zip(n_k["n"], n_k["k"]):
+
+        tf_datasets = ann.loc[ann["Dataset type"].isin(["Training", "Filtration"]), "Dataset"].unique()
+        tf_num = classifiers_num(out, main_scoring_function, main_scoring_threshold, tf_datasets)
+
+        tfv_datasets = ann.loc[ann["Dataset type"].isin(["Training", "Filtration", "Validation"]), "Dataset"].unique()
+        tfv_num = classifiers_num(out, main_scoring_function, main_scoring_threshold, tfv_datasets)
+
+        table = table.append( { 'n'                 : n, 
+                                'k'                 : k, 
+                                'train_filtr'       : tf_num,
+                                'train_filtr_valid' : tfv_num,
+                                'percent'           : 100 * tfv_num / tf_num }, ignore_index=True)
+    print(table)
+
+    table = pd.DataFrame(columns=['gene','percent'])
+    clsfs_num = out.shape[0]
+    genes_to_clsfs_num = {}
+    for clsf in out.index:
+        for g in clsf.split(';'):
+            if g in genes_to_clsfs_num:
+                genes_to_clsfs_num[g] += 1
+            else:
+                genes_to_clsfs_num[g] = 1
+    for g in genes_to_clsfs_num:
+        table = table.append( { 'gene'    : g, 
+                                'percent' : 100 * genes_to_clsfs_num[g] / clsfs_num }, ignore_index=True)
+    table.sort_values(by=['percent'])
+    print(table)
