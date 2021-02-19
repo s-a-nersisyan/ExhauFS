ce66a2aa8c6fdff48d22ef07521ca75f7c925330
s-a-nersisyan <s.a.nersisyan@gmail.com>
2021-02-19 22:30:08 +0000

    Script for time estimation simplified

--- build_classifiers.py
+++ build_classifiers.py
@@ -12,12 +12,23 @@ from core import classifiers
 from core import accuracy_scores
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please specify configuration file", file=sys.stderr)
-        sys.exit(1)
+def load_config_and_input_data(config_path, load_n_k=True):
+    """Load configuration file and input data
     
-    config_path = sys.argv[1]
+    Parameters
+    ----------
+    config_path : string
+        Path to config file (json).
+    load_n_k : bool
+        Whether load n_k table or not.
+
+    Returns
+    -------
+    dict, pd.DataFrame, pd.DataFrame, pd.DataFrame
+        Dict with configuration, data (df), annotation (ann) and 
+        table with n, k pairs (n_k).
+    """
+
     try:
         config_file = open(config_path, "r")
     except:
@@ -35,10 +46,41 @@ if __name__ == "__main__":
     config_dirname = os.path.dirname(config_path)
     df = pd.read_csv(os.path.join(config_dirname, config["data_path"]), index_col=0)
     ann = pd.read_csv(os.path.join(config_dirname, config["annotation_path"]), index_col=0)
-    n_k = pd.read_csv(os.path.join(config_dirname, config["n_k_path"]))
-    main_scoring_function = config["main_scoring_function"]
-    main_scoring_threshold = config["main_scoring_threshold"]
+    if load_n_k:
+        n_k = pd.read_csv(os.path.join(config_dirname, config["n_k_path"]))
+    else:
+        n_k = pd.DataFrame()
 
+    return config, df, ann, n_k
+
+
+def run_exhaustive_classification(config, df, ann, n_k):
+    """Run the pipeline for classifier construction
+    using exhaustive feature selection.
+    
+    Parameters
+    ----------
+    config : dict
+        Configuration dictionary.
+    df : pandas.DataFrame
+        A pandas DataFrame whose rows represent samples
+        and columns represent features.
+    ann : pandas.DataFrame
+        DataFrame with annotation of samples. Three columns are mandatory:
+        Class (binary labels), Dataset (dataset identifiers) and 
+        Dataset type (Training, Filtration, Validation).
+    n_k : pandas.DataFrame
+        DataFrame with columns n and k defining a grid
+        for exhaustive feature selection: n is a number
+        of selected features, k is a length of each
+        features subset.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame with constructed classifiers and their
+        quality scores.
+    """
     model = classification.ExhaustiveClassification(
         df, ann, n_k,
         getattr(feature_pre_selectors, config["feature_pre_selector"] or "", None),
@@ -56,8 +98,23 @@ if __name__ == "__main__":
         random_state=config["random_state"],
         verbose=config["verbose"]
     )
-    res = model.exhaustive_run()
+    return model.exhaustive_run()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please specify configuration file", file=sys.stderr)
+        sys.exit(1)
     
+    # Load config and input data
+    config_path = sys.argv[1]
+    config, df, ann, n_k = load_config_and_input_data(config_path)
+    
+    # Build classifiers
+    res = run_exhaustive_classification(config, df, ann, n_k)
+    
+    # Save raw results (classifiers and their quality scores)
+    config_dirname = os.path.dirname(config_path)
     output_dir = os.path.join(config_dirname, config["output_dir"])
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -79,7 +136,11 @@ if __name__ == "__main__":
         # validation datasets (i.e. all datasets)
         all_datasets = ann["Dataset"].unique()
         query_string = " & ".join([
-            "(`{};{}` >= {})".format(ds, main_scoring_function, main_scoring_threshold) for ds in all_datasets
+            "(`{};{}` >= {})".format(
+                ds, 
+                config["main_scoring_function"], 
+                config["main_scoring_threshold"]
+            ) for ds in all_datasets
         ])
         all_num = len(res_n_k.query(query_string))
 
@@ -93,6 +154,7 @@ if __name__ == "__main__":
     summary_n_k["n"] = summary_n_k["n"].astype(int)
     summary_n_k["k"] = summary_n_k["k"].astype(int)
     summary_n_k["num_training_reliable"] = summary_n_k["num_training_reliable"].astype(int)
+    summary_n_k["num_validation_reliable"] = summary_n_k["num_validation_reliable"].astype(int)
     summary_n_k["percentage_reliable"] = summary_n_k["percentage_reliable"].astype(int)
     summary_n_k.to_csv("{}/summary_n_k.csv".format(output_dir), index=None)
     
--- time_n_k.py
+++ time_n_k.py
@@ -1,160 +1,94 @@
-import json
-import time
-import subprocess
-import pandas as pd
-import os
 import sys
-import random
+import os
+import time
+
+import pandas as pd
+
 from scipy.special import binom
-from itertools import product
-from functools import reduce
 
-script_path = 'build_classifiers.py'
+import build_classifiers
 
-class TimePredicter:
-    """
 
-    Parametors
+def predict_running_time(warm_up_n, warm_up_k, warm_up_time, grid_size, config):
+    """Estimate pipeline running time for different
+    values of n and k using data from a warm-up run
+    
+    Parameters
     ----------
-    script_path : string
-        Path to script
-    config_path : string
-        Path to configuration file for script
-    c : int (optional)
-        Multiplier for time asymptotic function
-    n_k_scout: {"n": [n_1, ..., n_m], "k": [k_1, ..., k_m]}
-        Little grid for calculating multiplier c
+    warm_up_n : int
+        Value of n used for warm-up run.
+    warm_up_k : int
+        Value of k used for warm-up run.
+    warm_up_time : float
+        Number of seconds used for warm-up run.
+    grid_size : int
+        Upper limit on values of n for which
+        function will make time predictions.
+    config : dict
+        Configuration dictionary.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame with three columns: n, k and estimated time.
+        The following grid is considered: 1 <= k <= n <= grid_size.
     """
+    # Create dataframe with predicted running time for each n, k
+    res = pd.DataFrame({
+        "n": [n for n in range(1, grid_size + 1) for k in range(1, n + 1)],
+        "k": [k for n in range(1, grid_size + 1) for k in range(1, n + 1)]
+    })
+    res["Estimated time"] = 0
     
-    def __init__(self, script_path, config_path, c = None, n_k_scout = {"n": [4], "k": [3]}):
-        self.script_path = script_path
-        self.config_path = config_path
-        self.config = self.read_config()
-        self.f_time = self.get_time_asymptotic_function()
-        self.n_k_scout = n_k_scout 
-        self.c = c if c is not None else self.get_time_modifier_by_scout()
-        print('Multiplier for asymptotic time function: {}'.format(self.c))
-        
-    def read_config(self):
-        config_file = open(self.config_path, "r")
-        config = json.load(config_file)
-        config_file.close()
-        return config
+    # Define complexity asymptotics for fitting
+    # specified classifiers
+    # TODO: fill this for other classifiers
+    if config["classifier"] == "SVC":
+        asymptotic_complexity = lambda n, k: k * binom(n, k)
     
-    def get_time_asymptotic_function(self):
-        return lambda n, k: k * binom(n, k)
+    # Find multiplicative complexity constant
+    # using complexity asymptotics and warm-up running time
+    C = warm_up_time / asymptotic_complexity(warm_up_n, warm_up_k)
     
-    def get_time_modifier_by_scout(self):
-        tmp_config_path, tmp_n_k_path = self.gen_modified_config(modified_n_k = self.n_k_scout)
-        real_time = self.get_script_real_time(new_config_path = tmp_config_path)
-        os.remove(tmp_config_path)
-        os.remove(tmp_n_k_path)
-        estimated_time = sum(map(self.f_time, self.n_k_scout["n"], self.n_k_scout["k"]))
-        c = real_time / estimated_time
-        return c
+    # Now do extrapolation
+    for i, n, k in zip(res.index, res["n"], res["k"]):
+        res.loc[i, "Estimated time"] = C * asymptotic_complexity(n, k)
     
-    def gen_modified_config(self, modified_n_k, config_filename = None, modified_n_k_filename = None):
-        
-        id_for_names = reduce(lambda x, y: str(x) + str(y), random.choices(range(10), k = 8)) # random id
-        if modified_n_k_filename is None:
-            modified_n_k_filename = 'n_k_{}.csv'.format(id_for_names)
-        if config_filename is None:
-            config_filename = 'config_{}.json'.format(id_for_names)
-        
-        config_dirname = os.path.dirname(self.config_path)
-        modified_n_k_path = os.path.join(config_dirname, modified_n_k_filename)
-        config_path = os.path.join(config_dirname, config_filename)
-        
-        n_k_df = pd.DataFrame(modified_n_k)
-        n_k_df.to_csv(modified_n_k_path, index=False)
-        
-        config = self.config.copy()
-        config["n_k_path"] = modified_n_k_filename
-        with open(config_path, 'w') as f:
-            json.dump(config, f)
-        return (config_path, modified_n_k_path)
-    
-    def get_script_real_time(self, new_config_path = None, verbose = False):
-        config_path = self.config_path if new_config_path is None else new_config_path
-        start_time = time.time()
-        if verbose:
-            subprocess.call(['python3', self.script_path, config_path])
-        else:
-            subprocess.call(['python3', self.script_path, config_path], stdout=subprocess.DEVNULL)
-        end_time = time.time()
-        return end_time - start_time
-    
-    def gen_time_table(self, size = 10, output = sys.stdout):
-        """
-        Generate table with estimated working time (in seconds) of script for all n, k <= size
-        
-        n\k 0 1 2 ... 
-        0   . . .
-        1   . . . 
-        2   . . .
-        .   . .
-        """
-        
-        def it(n, k): return 0 if k > n or k == 0 or n == 0 else int(self.c * self.f_time(n, k))
-    
-        tb_df = pd.DataFrame([[it(n, k) for k in range(size)] for n in range(size)])
-        tb_df.to_csv(output)
-        return tb_df
-    
-    def gen_n_k(self, t, strategy, basis, output_n_k = sys.stdout):
-        """Generate n_k grid by strategy from basis with working time t of script
-        
-        Parametors
-        ----------
-        t : int or string
-            Мax working time of script in seconds (TODO in format '%d%h%m%s')
-        strategy : string
-        basis : list
-            Variants of strategy
-                'k_to_n'
-                    Generate n_1, ..., n_p for basis = [k_1, ..., k_p]
-                'n,>k'
-                    Generate (n, k), (n, k+1), ... (n, k+p) for basis = [n, k]
-        """
-        if strategy == 'k_to_n':
-            # Find [n_1, ..., n_p] with max closer time working to t
-            t_0 = t / len(basis)
-            def lr_limits(P):
-                """Find pair (z, z+1) for linear predicate P(x) such that
-                    P(x) = false for x <= z
-                    P(x) = true for x >= z+1
-                """
-                z = 1
-                while not P(z):
-                    z += 1
-                if z == 1:
-                    print("Little max working time", file=sys.stderr)
-                    sys.exit(1)
-                return (z-1, z)
-            n_vars = []
-            for k in basis:
-                n_l, n_r = lr_limits(lambda n: self.c * self.f_time(n, k) > t_0)
-                n_vars.append((n_l, n_r))
-            #n_vars = map(lambda k: lr_limits(lambda n: c * f_time(n, k) > t_0), basis) #[(n_l, n_r), ..., ]
-            n_vars = list(product(*n_vars)) #[[n_1, ..., n_p], ...]
-            nk_vars = list(map(lambda lt: list(zip(lt, basis)), n_vars)) #[[(n_1, k_1), ..., (n_p, k_p)], [(n'_1, k_1), ..., (n'_p, k_p)], ...]
-            w_time = lambda gr: sum(list(map(lambda pr: self.c * self.f_time(pr[0], pr[1]), gr)))
-            nk_vars = list(filter(lambda it: w_time(it) <= t, nk_vars))
-            
-            nk = max(nk_vars, key = w_time)
-            n_k = {"n": list(map(lambda x: x[0], nk)), "k": basis}
-        elif strategy == 'n,>k':
-            pass #TODO
-        
-        n_k_df = pd.DataFrame(n_k)
-        n_k_df.to_csv(output, index=False)
-        
-        return n_k
+    return res
+
 
 if __name__ == "__main__":
-    config_path = sys.argv[1]
-    #config_path = './examples/example1/config.json'
-    size = int(sys.argv[2])
+    if len(sys.argv) < 2:
+        print("Please specify configuration file", file=sys.stderr)
+        sys.exit(1)
     
-    tm = TimePredicter(script_path, config_path)
-    tm.gen_time_table(size=10)
+    # Load config and input data
+    config_path = sys.argv[1] 
+    config, df, ann, _ = build_classifiers.load_config_and_input_data(config_path, load_n_k=False)
+    config["verbose"] = False
+    
+    # Values of n and k for warm-up
+    # TODO: allow to pass this values from command line (default: 50 and 2)
+    warm_up_n = 50
+    warm_up_k = 2
+    warm_up_n_k = pd.DataFrame({"n": [warm_up_n], "k": [warm_up_k]})
+
+    # Do warm-up run and measure the running time
+    start_time = time.time()
+    build_classifiers.run_exhaustive_classification(config, df, ann, warm_up_n_k)
+    end_time = time.time()
+    warm_up_time = end_time - start_time
+    
+    # Extrapolate warm-up running time to (n, k) grid
+    # Upper bound for n
+    # TODO: allow to pass this value from command line
+    grid_size = 100
+    res = predict_running_time(warm_up_n, warm_up_k, warm_up_time, grid_size, config)
+    
+    # Save the results
+    config_dirname = os.path.dirname(config_path)
+    output_dir = os.path.join(config_dirname, config["output_dir"])
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    res.to_csv("{}/time_estimates.csv".format(output_dir), index=None)
