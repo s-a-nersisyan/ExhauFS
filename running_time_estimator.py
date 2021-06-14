53eb67ef941bafd2465932df8f662f605d79699e
Victor Novosad <your_email@example.com>
2021-06-14 14:33:50 +0300

    rte fix

--- running_time_estimator.py
+++ running_time_estimator.py
@@ -1,14 +1,23 @@
+# External imports
+import sys
+import os
+import math
+import pandas as pd
+
+# Internal imports
 from utils import *
 
 
-def main(config_path, max_k, max_estimated_time, n_feature_subsets):
+def main(config_path, max_k, max_estimated_time, n_feature_subsets, is_regressor):
     config, df, ann, n_k = load_config_and_input_data(config_path, load_n_k=False)
     config["limit_feature_subsets"] = True
     config["shuffle_feature_subsets"] = True
     config["n_feature_subsets"] = n_feature_subsets
 
-    # model = initialize_classification_model(config, df, ann, n_k)
-    model = initialize_regression_model(config, df, ann, n_k)
+    if is_regressor:
+        model = initialize_regression_model(config, df, ann, n_k)
+    else:
+        model = initialize_classification_model(config, df, ann, n_k)
 
     def get_running_time(n, k):
         _, time = model.exhaustive_run_n_k(n, k)
@@ -59,5 +68,6 @@ if __name__ == "__main__":
     max_k = int(sys.argv[2])
     max_estimated_time = float(sys.argv[3])  # In hours
     n_feature_subsets = int(sys.argv[4])  # 100 is pretty good
+    is_regressor = int(sys.argv[5]) == 1 if len(sys.argv) > 5 else False  # 0 or 1
 
-    main(config_path, max_k, max_estimated_time, n_feature_subsets)
+    main(config_path, max_k, max_estimated_time, n_feature_subsets, is_regressor)
